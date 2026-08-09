a028c0f428abfc1d7a62bef338d138095791451b
Sahana devi R A <sahanadevi2037@gmail.com>
2026-08-09 11:44:33 +0530

    Implement insertBefore method for ArrayBasedList

---
+++ ArrayBasedList/insert before an element.cpp
@@ -0,0 +1,23 @@
+void arraylist::insertBefore(){
+    if(top == MAX - 1){
+        cout << "List is full";
+        return;
+    }int key, value;
+    cout << "Enter the element before which insertion is to be done: ";
+    cin >> key;
+    cout << "Enter the new element: ";
+    cin >> value;
+    int pos = -1;
+    for(int i = 0; i <= top; i++){
+        if(s[i].data == key){
+            pos = i;
+            break;
+        }
+    }if(pos == -1){
+        cout << "Element not found";
+        return;
+    }for(int i = top; i >= pos; i--){
+        s[i + 1].data = s[i].data;
+    }s[pos].data = value;
+    top++;
+}
