faef270afeccd657699401ddd449cce97da1ab04
Sahana devi R A <sahanadevi2037@gmail.com>
2026-08-09 10:57:08 +0530

    Create ADT.cpp

---
+++ ArrayBasedList/ADT.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+#define max 60
+class arraylist{
+  private:
+    int top;
+  public:
+    struct node{
+    long int data;
+    s[max];
+    arraylist(){
+        top = -1;
+    }
+void insert(int d);
+void display();
